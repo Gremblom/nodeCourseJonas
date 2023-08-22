600661bb723b6d8feabeb271df257ff50710bfca
Gremblom <cdflorezs@gmail.com>
2023-08-22 06:12:09 -0500

    Curso completo terminado

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,25 +1,87 @@
-const replaceTemplate = require('./modules/replaceTemplate');
-
 const slugify = require('sluify');
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
-// Debajo inicializacion const server
-const {query, pathName} = url.parse(req.url, true);
+const replaceTemplate = require('./modules/replaceTemplate');
+//////////////////////////////////
+// FILES
 
-/* Borramos todo de product page menos su res.end */
-resizeBy.writeHead(200, {'Content-Tye' : 'text/html'});
-const product = dataObj[query.id];
+// Blocking, syncrhonous way
+/* const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
 
-const output = replaceTemplate(tempProduct, product);
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log("File written!"); */
 
-resizeBy.end(output);
+// Non-blocking, asynchronous way
+/* fs.readFile('./txt/start.txt', 'utf-8', (err, data1)=>{
+    if (err) return console.log("ERROR!");
 
-// EN LA TEMPLATE DE PRODUCT
-<a href="/overview" class="product__back">
-    <span class="emoji-left"></span>Back
-</a>
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2)=>{
+        console.log(data2);
+        fs.readFile('./txt/append.txt', 'utf-8', (err, data3)=>{
+            console.log(data3);
 
-/*  PEGAR LA FUNCION REPLACETEMPLATE EN MODULES*/
+            fs.writeFile('./txt/final.txt',`${data2}\n${data3}`, 'utf-8', (err)=>{
+                console.log("Your file has been written");
+            })
+        });
+    });
+});
+console.log('Will read file!'); */
 
-// Encima de la delaracion del server
+/////////////////////////////////////////
+// SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
 const slugs = dataObj.map(el => slugify(el.productName, {lower : true}));
 console.log(slugs);
+
+const server = http.createServer((req, res)=>{
+    const {query, pathName} = url.parse(req.url, true);
+
+    // Overview page
+    if (pathName === '/' || pathName === '/overview'){
+        res.writeHead(404, {
+            'Content-Type':'text/html'});
+            const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+            const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+            
+
+        res.end(output);
+
+        // Product page
+    } else if (pathName === '/product'){
+        resizeBy.writeHead(200, {'Content-Tye' : 'text/html'});
+        const product = dataObj[query.id];
+
+        const output = replaceTemplate(tempProduct, product);
+
+        resizeBy.end(output);
+
+        // API
+    } else if (pathName === '/api'){  
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(data);   
+
+        // Not found
+    } else {
+        res.writeHead(404, {
+            'Content-Type':'text/html',    
+            'my-own-header':'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
+});
+
+server.listen(8000, '127.0.0.1', ()=>{
+    console.log("Listening to requests to requests on port 8000");
+})
