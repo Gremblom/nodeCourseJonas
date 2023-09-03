f81f82c2eb22a23d723232c8ce7748abd53a1198
Gremblom <cdflorezs@gmail.com>
2023-09-03 11:19:03 -0500

    Tarea semana2 Completa

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
+const hola = [2, 3];
+
 const readFilePro = file=>{
     return new Promise((resolve, reject)=>{
         fs.readFile(file, (err, data)=>{
@@ -11,16 +13,58 @@ const readFilePro = file=>{
     });
 }
 
-readFilePro(`${__dirname}/dog.txt`).then(data =>{
-    console.log(`Breed: ${data}`);
-
-    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).then(res =>{
-        console.log(res.body.message);
-
-        fs.writeFile('dog-img.txt', res.body.message, err =>{
-            console.log('Random dog image saved to file!');
+const writeFilePro = (file, data)=>{
+    return new Promise((resolve, reject)=>{
+        fs.writeFile(file, data, err =>{
+            if (err) reject('Could not write file');
+            resolve('success');
         });
-    }).catch(err =>{
+    });
+}
+
+const getDogPic = async ()=>{
+    try {
+        const imgs = [];
+
+        const data = await readFilePro(`${__dirname}/dog.txt`);
+
+        const str = data.toString();
+        const razas = str.split(',');
+
+        console.log(`Breed: ${razas}`);
+
+        razas.forEach(async (raza)=>{
+            try {
+                const res = await superagent.get(`https://dog.ceo/api/breed/${raza}/images/random`);
+                imgs.push(res.body.message);                
+                    
+                await writeFilePro('dog-img.txt', imgs.join('\n'));
+
+            } catch (error) {
+                console.log(error.message);
+            }
+        });
+
+        console.log('Random dog image saved to file!'); 
+
+    } catch (err) {
         console.log(err.message);
-    }); 
-});
+
+        throw err;
+    }
+
+    return '2: Ready!';
+}
+
+(async ()=>{
+    try {
+        console.log('1: Will get dog pics!');
+
+        const x = await getDogPic();
+        console.log(x);
+
+        console.log('3: Done getting dog pics!');
+    } catch (err) {
+        console.log(err.message);
+    }
+})();
